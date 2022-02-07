26b9655d902a14f63e56d87d9d51f0ee6b0993f4
David Jorgensen <r.d.jorgensen@protonmail.com>
2022-02-07 15:08:04 -0800

    Started A2

---
+++ MultiThreadSort/src/a2/djorgensen/Main.java
@@ -0,0 +1,11 @@
+package a2.djorgensen;
+
+import a1.djorgensen.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Food f = a1.djorgensen.Main.genFood();
+        System.out.println(f);
+    }
+}
--- invintory/src/a1/djorgensen/Main.java
+++ invintory/src/a1/djorgensen/Main.java
@@ -6,16 +6,16 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    private static final Random ran = new Random();
+    private static final FoodItems[] foodItems = FoodItems.values();
+    private static final Tools[] tools = Tools.values();
+    private static final ToolUses[] toolUses = ToolUses.values();
+    private static final Armors[] armors = Armors.values();
+    private static final Weapons[] weapons = Weapons.values();
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Random ran = new Random();
         List<Item> items = new ArrayList<>();
-        FoodItems[] foodItems = FoodItems.values();
-        Tools[] tools = Tools.values();
-        ToolUses[] toolUses = ToolUses.values();
-        Armors[] armors = Armors.values();
-        Weapons[] weapons = Weapons.values();
+        Scanner scan = new Scanner(System.in);
 
         System.out.print("How many items do you want?");
         int itemCnt = Integer.parseInt(scan.nextLine());
@@ -23,48 +23,52 @@ public class Main {
         for (int i = 0; i < itemCnt; i++) {
             int type = ran.nextInt(4);
             switch (type) {
-                case 0 -> {
-                    int foodIndex = ran.nextInt(foodItems.length);
-                    String foodName = foodItems[foodIndex].toString();
-                    float foodPrice = ran.nextFloat(10);
-                    int foodQty = ran.nextInt(30);
-                    int foodUses = ran.nextInt(6);
-                    float healthGain = ran.nextFloat(20);
-                    Food tmpFood = new Food(foodName, foodPrice, foodQty, foodUses, healthGain);
-                    items.add(tmpFood);
-                }
-                case 1 -> {
-                    int toolIndex = ran.nextInt(tools.length);
-                    String toolName = tools[toolIndex].toString();
-                    float toolPrice = ran.nextFloat(200);
-                    int toolQty = ran.nextInt(15);
-                    String use = toolUses[toolIndex].toString();
-                    Tool tmpTool = new Tool(toolName, toolPrice, toolQty, use);
-                    items.add(tmpTool);
-                }
-                case 2 -> {
-                    int armorIndex = ran.nextInt(armors.length);
-                    String armorName = armors[armorIndex].toString();
-                    float armorPrice = ran.nextFloat(2000);
-                    int armorQty = ran.nextInt(3);
-                    float defence = ran.nextFloat(1000);
-                    Armor tmpArmor = new Armor(armorName, armorPrice, armorQty, defence);
-                    items.add(tmpArmor);
-                }
-                case 3 -> {
-                    int weaponIndex = ran.nextInt(weapons.length);
-                    String weaponName = weapons[weaponIndex].toString();
-                    float weaponPrice = ran.nextFloat(1200);
-                    int weaponQty = ran.nextInt(5);
-                    float damage = ran.nextFloat(1000);
-                    Armor tmpWeapon = new Armor(weaponName, weaponPrice, weaponQty, damage);
-                    items.add(tmpWeapon);
-                }
+                case 0 -> items.add(genFood());
+                case 1 -> items.add(genTool());
+                case 2 -> items.add(genArmor());
+                case 3 -> items.add(genWeapon());
             }
         }
 
         for(Item i : items) {
             System.out.println(i);
         }
+    }
+
+    public static Food genFood() {
+        int foodIndex = ran.nextInt(foodItems.length);
+        String foodName = foodItems[foodIndex].toString();
+        float foodPrice = ran.nextFloat(10);
+        int foodQty = ran.nextInt(30);
+        int foodUses = ran.nextInt(6);
+        float healthGain = ran.nextFloat(20);
+        return new Food(foodName, foodPrice, foodQty, foodUses, healthGain);
+    }
+
+    public static Tool genTool() {
+        int toolIndex = ran.nextInt(tools.length);
+        String toolName = tools[toolIndex].toString();
+        float toolPrice = ran.nextFloat(200);
+        int toolQty = ran.nextInt(15);
+        String use = toolUses[toolIndex].toString();
+        return new Tool(toolName, toolPrice, toolQty, use);
+    }
+
+    public static Armor genArmor() {
+        int armorIndex = ran.nextInt(armors.length);
+        String armorName = armors[armorIndex].toString();
+        float armorPrice = ran.nextFloat(2000);
+        int armorQty = ran.nextInt(3);
+        float defence = ran.nextFloat(1000);
+        return new Armor(armorName, armorPrice, armorQty, defence);
+    }
+
+    public static Weapon genWeapon() {
+        int weaponIndex = ran.nextInt(weapons.length);
+        String weaponName = weapons[weaponIndex].toString();
+        float weaponPrice = ran.nextFloat(1200);
+        int weaponQty = ran.nextInt(5);
+        float damage = ran.nextFloat(1000);
+        return new Weapon(weaponName, weaponPrice, weaponQty, damage);
     }
 }
