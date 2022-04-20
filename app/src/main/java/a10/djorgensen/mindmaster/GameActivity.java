ff49c7bf2c76880eeb46b8c50a526ec7da96d329
David <r.d.jorgensen@gmail.com>
2022-04-20 15:09:16 -0700

    a10 done

---
+++ app/src/main/java/a10/djorgensen/mindmaster/GameActivity.java
@@ -0,0 +1,31 @@
+package a10.djorgensen.mindmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Point;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+    private GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Point screenSize = new Point();
+        getWindowManager().getDefaultDisplay().getSize(screenSize);
+        gameView = new GameView(this, screenSize);
+        setContentView(gameView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resume();
+    }
+}
---
+++ app/src/main/java/a10/djorgensen/mindmaster/GameView.java
@@ -0,0 +1,75 @@
+package a10.djorgensen.mindmaster;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView implements Runnable {
+    private final int FPS = 1000/30;
+    private Thread thread;
+    private boolean isPlaying;
+    private Board board;
+
+    public GameView(Context context, Point screenSize) {
+        super(context);
+        board = new Board(screenSize, getResources());
+    }
+
+    @Override
+    public void run() {
+        while(isPlaying){
+            update();
+            draw();
+            sleep();
+        }
+    }
+
+    public void update() {
+
+    }
+
+    public void draw() {
+        if(getHolder().getSurface().isValid()) {
+            Canvas canvas = getHolder().lockCanvas();
+            canvas.drawColor(Color.BLACK);
+            board.draw(canvas);
+            getHolder().unlockCanvasAndPost(canvas);
+        }
+    }
+
+    public void pause() {
+        isPlaying = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void resume() {
+        isPlaying = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public void sleep() {
+        try {
+            thread.sleep(FPS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            int x = Math.round(event.getX());
+            int y = Math.round((event.getY()));
+            board.onClick(x, y);
+        }
+        return true;
+    }
+}
